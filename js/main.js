4ae3500ec4fa40c712d3c0eb2cf82e1cf72e9647
Bryce Marrett <b_marrett@outlook.com>
2025-08-15 16:15:00 +1000

    final push

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 const blurbs = {
-    fear: { text: "Fear drives humans to innovate or destroy.", image: "./assets/images/fear.jpg" },
+    fear: { text: "Fear drives humans to innovate or destroy.", image: "./assets/images/fear.png" },
     mind: { text: "The mind is the ultimate frontier.", image: "./assets/images/head.png" }, // skull with tech implants
-    overpopulation: { text: "Over Population challenges resources.", image: "./assets/images/overpopulation.jpg" },
+    overpopulation: { text: "Over Population challenges resources.", image: "./assets/images/overpopulation.png" },
     superhuman: { text: "Super Human capabilities extend beyond biology.", image: "./assets/images/arm.png" }, // an arm with technology and muscle image
-
-    economy: { text: "The economy evolves alongside automation.", image: "./assets/images/economy.jpg" },
+    economy: { text: "The economy evolves alongside automation.", image: "./assets/images/economy.png" },
     utopia: { text: "Utopia represents the ultimate vision.", image: "./assets/images/man.png" }, //image is of a peruvian man with tech augments
-    war: { text: "War shifts into cyber realms.", image: "./assets/images/war.jpg" },
-    simuology: { text: "Simuology studies the simulated realities.", image: "./assets/images/simuology.jpg" },
-    simulation: { text: "Simulation experiments model societies.", image: "./assets/images/simulation.jpg" }
+    war: { text: "War shifts into cyber realms.", image: "./assets/images/war.png" },
+    simuology: { text: "Simuology studies the simulated realities.", image: "./assets/images/simuology.png" },
+    simulation: { text: "Simulation experiments model societies.", image: "./assets/images/simulation.png" }
 };
 
 const wordElements = document.querySelectorAll(".word");
